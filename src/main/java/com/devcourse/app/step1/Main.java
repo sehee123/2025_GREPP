9562139a385bf9866f6d89c5be9616327c1c237e
sehee <se_hee__naver.com>
2025-03-22 16:31:50 +0900

    step1-5

--- src/main/java/com/devcourse/app/step1/Main.java
+++ src/main/java/com/devcourse/app/step1/Main.java
@@ -30,11 +30,21 @@ public class Main {
 
                 board.push(arr);
             }else if(command.equals("조회")){
-                System.out.println("제목 : [" + board.peek()[0] + "]");
-                System.out.println("내용 : [" + board.peek()[1] + "]");
+                System.out.print("어떤 게시물을 조회할까요? " );
+                int n = Integer.parseInt(sc.next());
+
+                System.out.println("제목 : [" + board.get(n-1)[0] + "]");
+                System.out.println("내용 : [" + board.get(n-1)[1] + "]");
             }else if(command.equals("삭제")){
-                board.pop();
+                System.out.print("어떤 게시물을 삭제할까요? " );
+                int n = Integer.parseInt(sc.next());
+
+                board.remove(n-1);
+                System.out.println(n + "번 게시물이 성공적으로 삭제되었습니다!");
             }else if (command.equals("수정")){
+                System.out.println("어떤 게시물을 수정할까요? " );
+                int n = Integer.parseInt(sc.next());
+                System.out.print(n + "번 게시물을 수정합니다" );
                 String[] arr = new String[2];
                 System.out.print("제목 : ");
                 String title = sc.next();
@@ -43,8 +53,8 @@ public class Main {
 
                 arr[0] = title;
                 arr[1] = content;
-                board.pop();
-                board.push(arr);
+                board.remove(n-1);
+                board.add(n-1 , arr);
             }
         }
 
